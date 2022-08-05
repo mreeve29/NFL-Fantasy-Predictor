b02b587eba88744bd2ecc16bbc9d316504fba094
mreeve29 <michael.s.reeve@gmail.com>
2022-08-05 16:18:41 -0400

    add player name to response

--- Notebooks/PythonScripts/nfl_db.py
+++ Notebooks/PythonScripts/nfl_db.py
@@ -2,14 +2,14 @@ import requests
 import pandas as pd
 
 def get_player_df(name):
-    url = "https://nfl-fantasy-predictor-db.herokuapp.com/player/df?&name=" + name
+    url = "https://nfl-fantasy-predictor-db.herokuapp.com/player/df_json?&name=" + name
     response = requests.get(url)
     data = response.json()
     if "error" in data:
         print("Error: " + data["error"])
         return None
     else:
-        return pd.read_json(data["response"])
+        return (data["player_name"], pd.read_json(data["response"]))
 
 def get_all_players_df():
     url = "https://nfl-fantasy-predictor-db.herokuapp.com/all_players_df"
